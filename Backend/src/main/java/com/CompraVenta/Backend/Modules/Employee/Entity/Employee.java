641e723eada594ae17d4f5213f7a1e38fa69c257
Admin <marceldgr@gmail.com>
2026-06-03 00:30:52 -0500

    creacion del Docker-Compose.yml

--- Backend/src/main/java/com/CompraVenta/Backend/Modules/Employee/Entity/Employee.java
+++ Backend/src/main/java/com/CompraVenta/Backend/Modules/Employee/Entity/Employee.java
@@ -58,7 +58,7 @@ public class Employee {
     private LocalDateTime updatedAt;
 
 
-    public static boolean isAdmin() {
+    public boolean isAdmin() {
         return Role.ADMIN.equals(this.role);
     }
 
